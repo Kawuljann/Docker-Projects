e1015359145b7050a4b10dd3b748f6da45fda51f
kawuli_abudureyimu <kawuli_abudureyimu@dsd.dokidoki-gw.com>
2022-11-03 00:30:38 +0900

    docker project

---
+++ docker-sql/database_students.sql
@@ -0,0 +1,12 @@
+use db;
+
+
+CREATE TABLE students(
+      StudentID int not null AUTO_INCREMENT,
+      FirstName varchar(100) NOT NULL,
+      Surname varchar(100) not NULL,
+      PRIMARY KEY (StudentID)
+);
+
+INSERT INTO students(FirstName, Surname)
+VALUES("AhmadJan", "Kasim"), ("UyghurJan", "Abduryim");
